9b6abd664fb0e07d5925ab52365e0cdb7a2a5433
Porsche <herskorn@gmail.com>
2024-07-12 18:52:55 -0700

    removed duplicate router rendering

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App.jsx';
 
 
-ReactDOM.createRoot(
-  <React.StrictMode>
-    <HashRouter basename="/">
-    <App />
-    </HashRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>\
+      <App />
+\  </React.StrictMode>
+)
